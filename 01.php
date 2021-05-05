3ca7b909fc06dcfb638949da62766ddf2c78bfa4
hasnain <teachingtechzone@gmail.com>
2021-05-05 15:45:39 +0500

    initilize repository

---
+++ 01.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+ 
+<?php
+
+// class 01
+//  echo "abc" . "<br>";
+//  $name = "Bilal";
+//  var_dump($name);
+//  $age = 14;
+//  var_dump($age);
+
+/*
+define('PI', 3.142);
+echo PI.'<br>';
+*/
+
+// // Create simple string
+// $string1 = "Hello, I am hasnain"; 
+// $string2 = 'Hello, I am bilal'; 
+// // echo $string1 . '<br>';
+// // echo $string2 . '<br>';
+
+// echo $string1 ." ". $string2;
+// $string = "    Hello World      ";
+
+// echo "2 - " . trim($string) . '<br>';
+
+
+// echo "1 - " . strlen($string) . '<br>'
+
+// echo "7 - " . strtoupper($string) . '<br>';
+
+
+// $price = -12;
+// $age = 5;
+
+// if($price == 12 || $age == 5) {
+//     echo "You can buy this kandy";
+
+// }
+// else{
+//     echo "You have not enogh money";
+// }
+
+// $num = 5 ;
+
+// if($num === "5"){
+//     echo "they are equal";
+// }
+// else{
+//     echo "they are not equal";
+// }
+// switch
+
+
+// $userRole = 'user'; // admin, editor, user
+
+// switch ($userRole) {
+//     case 'admin':
+//         echo 'Admin, You can do anything<br>';
+//         break;
+//     case 'editor';
+//         echo 'Editor, You can edit content<br>';
+//         break;
+//     case 'user':
+//         echo 'user, You can view posts and comment<br>';
+//         break;
+//     default:
+//         echo 'Unknown, role<br>';
+// }
+
+
+
+
+
+?>
+</body>
+</html>
