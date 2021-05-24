517ca3afb77f4c16c4df872bc548ac1f68c4d9fc
hasnain <teachingtechzone@gmail.com>
2021-05-24 16:28:26 +0500

    class 7th ended with half file system

---
+++ class7.php
@@ -0,0 +1,39 @@
+<?php
+// // Magic constants, they change their values based on their execution
+// echo __FILE__ . '<br>';
+// echo __DIR__ . '<br>';
+// echo __LINE__. '<br>';
+
+// // Create directory
+// mkdir('class7test');
+
+// Rename directory
+// rename('class7test', 'class7test1');
+
+// Delete directory
+// rmdir('class7test1');
+
+// // Read files and folders inside current directory
+// $files = scandir('./');
+// echo '<pre>';
+// var_dump($files);
+// echo '</pre>';
+
+
+// // // Read files and folders inside parent directory
+// $files = scandir('../');
+// echo '<pre>';
+// var_dump($files);
+// echo '</pre>';
+
+
+// // // file_get_contents, file_put_contents
+
+// echo  file_get_contents('class7.txt'); // get content of file
+
+// $lorem = file_get_contents('class7.txt'); // get content of file
+// echo $lorem;
+// echo '<br>';
+
+// file_put_contents('class7.txt', "First line " . $lorem);
+
