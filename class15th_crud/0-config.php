ca0e4cd707f0dae43cfb8bcb590876ab2224bc6a
hasnain <hasnainkhankqs@gmail.com>
2021-06-17 11:39:25 +0500

    adding crud connection and insertion

---
+++ class15th_crud/0-config.php
@@ -0,0 +1,92 @@
+<?php
+
+
+
+
+
+
+
+// // // Mysqli Object Oriented
+// // // /* Database credentials. Assuming you are running MySQL
+// // // server with default setting (user 'root' with no password) */
+// define('DB_SERVER', 'localhost');
+// define('DB_USERNAME', 'root');
+// define('DB_PASSWORD', '');
+// define('DB_NAME', 'simpledb');
+ 
+// /* Attempt to connect to MySQL database */
+// $conn = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+ 
+// // Check connection
+// if ($conn->connect_error) {
+//     die("Connection failed: " . $conn->connect_error);
+//   }
+
+// else{
+//       echo "connection working fine </br>";
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// //Mysqli procedural
+
+// /* Database credentials. Assuming you are running MySQL
+// server with default setting (user 'root' with no password) */
+// define('DB_SERVER', 'localhost');
+// define('DB_USERNAME', 'root');
+// define('DB_PASSWORD', '');
+// define('DB_NAME', 'simpledb');
+ 
+// /* Attempt to connect to MySQL database */
+// $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+ 
+// // Check connection
+// if(!$conn){
+//     die("ERROR: Could not connect. " . mysqli_connect_error());
+// }
+// else{
+    // echo "connection working fine </br>";
+// }
+
+
+
+
+
+
+
+
+//PDO (PHP Data Objects)
+// to read more about PDO 
+
+// // /* Database credentials. Assuming you are running MySQL
+// // // server with default setting (user 'root' with no password) */
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'simpledb');
+ 
+/* Attempt to connect to MySQL database */
+try{
+    $conn = new PDO("mysql:host=" . DB_SERVER . ";dbname=" . DB_NAME, DB_USERNAME, DB_PASSWORD);
+    // Set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // Basically, we here are setting the PDO error mode attribute (controls error reporting)
+    // to raise a PDOException that tells you what went wrong when it goes wrong.
+} catch(PDOException $e){
+    die("ERROR: Could not connect. " . $e->getMessage());
+}
+
+?>
---
+++ class15th_crud/1-insert.php
@@ -0,0 +1,77 @@
+<?php
+require_once "0-config.php";
+
+// // // By MySqli OOP
+
+// $sql = "INSERT INTO employees (name, alary) VALUES ('ahsan', 500090)";
+
+// echo $conn->query($sql);
+
+// if ($conn->query($sql) === TRUE) {
+//     // echo '<pre>';
+//     // print_r($conn);
+//     // echo '</pre>';
+//   $last_id = $conn->insert_id;
+//   echo "New record created successfully by OOP. Last inserted ID is: " . $last_id;
+// } else {
+//     echo "Error: " . $sql . "<br>" . $conn->error;
+// }
+
+// $conn->close();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // By MySqli procedural
+// $sql = "INSERT INTO employees (name, salary) VALUES ('Nehal', 0)";
+
+// if (mysqli_query($conn, $sql)) {
+//   $last_id = mysqli_insert_id($conn);
+//   echo "New record created successfully by procedural. Last inserted ID is: " . $last_id;
+// } else {
+//   echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+// }
+// mysqli_close($conn);
+
+
+
+
+
+
+
+
+// By PDO procedural
+try {
+
+    $sql = "INSERT INTO employees (name, salary) VALUES ('abc', 700)";
+    // use exec() because no results are returned   
+    $conn->exec($sql);
+    $last_id = $conn->lastInsertId();
+    echo "New record created successfully by pdo. Last inserted ID is: " . $last_id;
+  } catch(PDOException $e) {
+    echo $sql . "<br>" . $e->getMessage();
+  }
+  
+  $conn = null;
+
+
+
+
+
+
+
+
+
+
+
+?>
